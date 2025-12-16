d510f4d747f65881516f8cb4ed472f88116f2233
Netanel Harush <netanelharush2@gmail.com>
2025-12-16 09:17:35 +0200

    Refactor login tests and update package type to module

--- tests/loginTests.spec.js
+++ tests/loginTests.spec.js
@@ -1,50 +1,37 @@
-import { test } from '@playwright/test';
-import { LoginPage } from '../pages/login.page';
-import { InventoryPage } from '../pages/inventory.page';
-import {
-  USERS,
-  POSITIVE_LOGIN_USERS,
-  NEGATIVE_LOGIN_USERS,
-} from '../data/users';
+import {test} from '@playwright/test'
+import {NEGATIVE_LOGIN_USERS, POSITIVE_LOGIN_USERS, USERS} from '../data/users'
+import {InventoryPage} from '../pages/inventory.page'
+import {LoginPage} from '../pages/login.page'
 
-test.describe('Login Tests', () => {
-
-  // ---------- Positive Login Tests ----------
+test.describe('Valid Login Tests', () => {
   for (const userKey of POSITIVE_LOGIN_USERS) {
-    test(`Positive login - ${userKey}_user`, async ({ page }) => {
-      const loginPage = new LoginPage(page);
-      const inventoryPage = new InventoryPage(page);
-      const user = USERS[userKey];
-
-      await loginPage.navigate();
-      await loginPage.login(user.username, user.password);
-
-      await inventoryPage.assertPageLoaded();
-    });
+    test(`Login with ${userKey} user`, async ({page}) => {
+      const loginPage = new LoginPage(page)
+      const inventoryPage = new InventoryPage(page)
+      const user = USERS[userKey]
+      await loginPage.navigate()
+      await loginPage.login(user.username, user.password)
+      await inventoryPage.assertPageLoaded()
+    })
   }
+})
 
-  // ---------- Negative Login Tests ----------
-  test('Negative login - locked_out_user', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-
-    await loginPage.navigate();
-    await loginPage.login(USERS.locked.username, USERS.locked.password);
-
+test.describe('Invalid Login Tests', () => {
+  test('login with locked out user', async ({page}) => {
+    const loginPage = new LoginPage(page)
+    await loginPage.navigate()
+    await loginPage.login(USERS.locked.username, USERS.locked.password)
     await loginPage.assertErrorMessage(
-      'Epic sadface: Sorry, this user has been locked out.'
-    );
-  });
+      'Epic sadface: Sorry, this user has been locked out.',
+    )
+  })
 
-  // ---------- Negative Login Tests (Data-driven) ----------
   for (const testData of NEGATIVE_LOGIN_USERS) {
-    test(`Negative login - ${testData.description}`, async ({ page }) => {
-      const loginPage = new LoginPage(page);
-
-      await loginPage.navigate();
-      await loginPage.login(testData.username, testData.password);
-
-      await loginPage.assertErrorMessageVisible();
-    });
+    test(`Login with ${testData.description}`, async ({page}) => {
+      const loginPage = new LoginPage(page)
+      await loginPage.navigate()
+      await loginPage.login(testData.username, testData.password)
+      await loginPage.assertErrorMessageVisible()
+    })
   }
-
-});
+})
--- tests/sanityTests.spec.js
+++ tests/sanityTests.spec.js
@@ -1,44 +1,44 @@
-import { test } from '@playwright/test';
-import { LoginPage } from '../pages/login.page';
-import { InventoryPage } from '../pages/inventory.page';
-import { CartPage } from '../pages/cart.page';
-import { CheckoutStepOnePage } from '../pages/checkoutStepOne.page';
-import { CheckoutStepTwoPage } from '../pages/checkoutStepTwo.page';
-import { CheckoutCompletePage } from '../pages/checkoutComplete.page';
-import { USERS } from '../data/users';
+import {test} from '@playwright/test'
+import {USERS} from '../data/users'
+import {CartPage} from '../pages/cart.page'
+import {CheckoutCompletePage} from '../pages/checkoutComplete.page'
+import {CheckoutStepOnePage} from '../pages/checkoutStepOne.page'
+import {CheckoutStepTwoPage} from '../pages/checkoutStepTwo.page'
+import {InventoryPage} from '../pages/inventory.page'
+import {LoginPage} from '../pages/login.page'
 
-test('Sanity test - full purchase flow', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  const inventoryPage = new InventoryPage(page);
-  const cartPage = new CartPage(page);
-  const stepOne = new CheckoutStepOnePage(page);
-  const stepTwo = new CheckoutStepTwoPage(page);
-  const completePage = new CheckoutCompletePage(page);
+test('Sanity test - full purchase flow', async ({page}) => {
+  const loginPage = new LoginPage(page)
+  const inventoryPage = new InventoryPage(page)
+  const cartPage = new CartPage(page)
+  const stepOne = new CheckoutStepOnePage(page)
+  const stepTwo = new CheckoutStepTwoPage(page)
+  const completePage = new CheckoutCompletePage(page)
 
   // Login
-  await loginPage.navigate();
-  await loginPage.login(USERS.standard.username, USERS.standard.password);
-  await inventoryPage.assertPageLoaded();
+  await loginPage.navigate()
+  await loginPage.login(USERS.standard.username, USERS.standard.password)
+  await inventoryPage.assertPageLoaded()
 
   // Add products
-  await inventoryPage.addTwoProducts();
-  await inventoryPage.assertCartBadge(2);
+  await inventoryPage.addTwoProducts()
+  await inventoryPage.assertCartBadge(2)
 
   // Cart
-  await inventoryPage.goToCart();
-  await cartPage.assertPageLoaded();
-  await cartPage.assertCartItemsCount(2);
+  await inventoryPage.goToCart()
+  await cartPage.assertPageLoaded()
+  await cartPage.assertCartItemsCount(2)
 
   // Checkout step one
-  await cartPage.checkout();
-  await stepOne.assertPageLoaded();
-  await stepOne.fillForm();
+  await cartPage.checkout()
+  await stepOne.assertPageLoaded()
+  await stepOne.fillForm()
 
   // Checkout step two
-  await stepTwo.assertPageLoaded();
-  await stepTwo.finish();
+  await stepTwo.assertPageLoaded()
+  await stepTwo.finish()
 
   // Complete
-  await completePage.assertPageLoaded();
-  await completePage.assertThankYouVisible();
-});
+  await completePage.assertPageLoaded()
+  await completePage.assertThankYouVisible()
+})
