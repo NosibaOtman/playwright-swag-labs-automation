6cff35da089d42fa722c1aef40d02e885a17e893
Nosiba <150847064+NosibaOtman@users.noreply.github.com>
2025-12-16 21:13:17 +0200

    16/12

---
+++ data/urls.js
@@ -0,0 +1,10 @@
+export const BASE_URL = 'https://www.saucedemo.com';
+
+export const URLS = {
+  login: `${BASE_URL}/`,
+  inventory: `${BASE_URL}/inventory.html`,
+  cart: `${BASE_URL}/cart.html`,
+  checkoutStepOne: `${BASE_URL}/checkout-step-one.html`,
+  checkoutStepTwo: `${BASE_URL}/checkout-step-two.html`,
+  checkoutComplete: `${BASE_URL}/checkout-complete.html`,
+};
